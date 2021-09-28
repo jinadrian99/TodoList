5c8baa82d6439cbe3038c2a50d9b0a41ab1f54ce
jinadrian99 <hanquocadrian@gmail.com>
2021-09-28 18:56:54 +0700

    for Vercel

--- Routes/person.js
+++ Routes/person.js
@@ -3,6 +3,24 @@ const mongoose = require('mongoose');
 const person = require('../Models/person');
 const router = express.Router();
 
+router.post('/sign-in/', async (req, res) => {
+    // try {
+    //     var email = req.body.email;
+    //     var password = req.body.password;
+
+    //     const objPerson = await person.findOne({ "email": req.body.email });
+    //     return res.send(objPerson);
+    // } catch (error) {
+    //     return res.send("Fail");
+    // }
+    try {
+        const obj = await person.findOne({ "email": req.body.email, "password": req.body.password });
+        return res.send(obj ? obj : "Fail");
+    } catch (error) {
+        return res.send("Fail");
+    }
+})
+
 router.get('/', async (req, res) => {
     try {
         const persons = await person.find();
@@ -24,8 +42,19 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const obj = await new person(req.body).save();
-        return res.send(obj);
+        //Whare mà không phải id -> tìm ko thấy sẽ ko có obj, nếu là id -> tìm ko thấy sẽ vào try catch
+        const obj = await person.findOne({ "email": req.body.email });
+        if(obj) {
+            return res.send("Exist");
+        } else {
+            // return res.send("Make");
+            try {
+                const obj = await new person(req.body).save();
+                return res.send(obj);
+            } catch (error) {
+                return res.send(error);
+            }
+        }
     } catch (error) {
         return res.send(error);
     }
--- server.js
+++ server.js
@@ -4,23 +4,23 @@ const cors = require('cors');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 
-// const connection = require('./Config/db');
-// connection();
+const connection = require('./Config/db');
+connection();
 
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// const project = require('./Routes/project');
-// const task = require('./Routes/task');
-// const person = require('./Routes/person');
+const project = require('./Routes/project');
+const task = require('./Routes/task');
+const person = require('./Routes/person');
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
 
-// app.use("/api/persons", person);
-// app.use("/api/projects", project);
-// app.use("/api/tasks", task);
+app.use("/api/persons", person);
+app.use("/api/projects", project);
+app.use("/api/tasks", task);
 
 app.listen(process.env.APP_PORT, () => {
     console.log("Server running on http://127.0.0.1:"+ process.env.APP_PORT +"...");
